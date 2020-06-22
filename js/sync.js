a3c611efc8167568054038599dff017f3700b783
Matt Zhou <mattzh72@mgmail.com>
2020-06-22 10:02:51 -0700

    Added abbreviation for numbers

--- js/sync.js
+++ js/sync.js
@@ -20,7 +20,7 @@ var db = firebase.firestore();
 
 function getNumEmails() {
     return db.collection(COLLECTION).doc(DOCUMENT).get().then(doc => {
-        return doc.data()[FIELD];
+        return abbreviateNumber(doc.data()[FIELD], 0);
     });
 }
 
@@ -33,6 +33,18 @@ function incrementEmailCount(num=1) {
             [FIELD]: newTotal
         });
         
-        return newTotal;
+        return abbreviateNumber(newTotal, 0);
     });
+}
+
+function abbreviateNumber(num, fixed) {
+  if (num === null) { return null; } // terminate early
+  if (num === 0) { return '0'; } // terminate early
+  fixed = (!fixed || fixed < 0) ? 0 : fixed; // number of decimal places to show
+  var b = (num).toPrecision(2).split("e"), // get power
+      k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+      c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3) ).toFixed(1 + fixed), // divide by power
+      d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+      e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+  return e;
 }
